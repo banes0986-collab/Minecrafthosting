0f488b3213ea9c3591aa86432bb344aaf5fc8a22
banes0986-collab <banes0986@gmail.com>
2026-05-10 01:53:21 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const SERVER_SETTINGS = {
+    serverIP: "legacynw.craftmc.com.tr",
+    discordLink: "https://discord.gg/EagYzrV4N",
+    adminUser: "triggerbabaa",
+    adminPass: "resul3163",
+    siteName: "Legacy Network",
+    version: "v2.0.4"
+};
