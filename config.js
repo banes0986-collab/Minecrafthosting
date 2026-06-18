bf6f80b9d5d07f65d0771671d02b49256d59f1e3
banes0986-collab <banes0986@gmail.com>
2026-06-18 21:09:53 +0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const CONFIG = {
-    serverIP: "legacynw.craftmc.com.tr",
-    discordWebhook: "BURAYA_YAPISTIR", // Discord'dan kopyaladığın link
-    discordLink: "https://discord.gg/EagYzrV4N"
-};
